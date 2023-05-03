0fe6b7bbb313c4d279ed45151f4ba685a0fb39e5
Rodrigo <rodrigo.rivera.12357@gmail.com>
2023-05-02 20:18:13 -0600

    generacion de pdf

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,27 @@ let { add, findOne, update, findAll } = require("../DBManger/mongo");
 const Joi = require('@hapi/joi');
 let Config = require('../Config/config').server.jwt;
 let jwt = require('jsonwebtoken');
+var path = require('path');
+const fs = require('fs')
+var pdf = require('html-pdf');
+var QRCode = require('qrcode')
 
+var options = {
+  format: "A3",
+  orientation: "portrait",
+
+  header: {
+    height: "8mm",
+
+  },
+  footer: {
+    height: "8mm",
+    contents: {
+      default: '<span style="color: #444;">{{page}}</span>/<span>{{pages}}</span>', // fallback value
+
+    }
+  }
+};
 
 
 
@@ -19,6 +39,12 @@ let data = Joi.object().keys({
   envio: Joi.number().integer().required(),
 });
 
+//{ "id": 70, "nombre": "Puños Azul", "etiquetas": 25 }
+let dataQr = Joi.array().items({
+  id: Joi.number().integer().required(),
+  nombre: Joi.string().min(3).max(255).required(),
+  etiquetas: Joi.number().integer().required(),
+})
 
 router.post('/venta', validateToken, async function (req, res, next) {
   const firm = "[index:venta] "
@@ -107,6 +133,80 @@ router.get('/aticulo/search/:articulo', async function (req, res, next) {
   }
 });
 
+router.get('/pdf/qr', async (req, res, next) => {
+
+  const { error } = dataQr.validate(req.body)
+  if (error) {
+    return res.status(400).json(
+      { code: 10, "mensaje": error.details[0].message }
+    )
+  }
+  let request = req.body;
+
+
+  let dir = path.join(__dirname, "../public/images/qr");
+  const files = await fs.promises.readdir(dir);
+  //console.log("at", files)
+
+  request.map(t => {
+    if (fs.existsSync(dir + "/" + t.id + ".png")) {
+      console.log("existe " + dir + "/" + t.id + ".png")
+    } else {
+      let dataQR = dir + "/" + t.id + ".png";
+      console.log("url qr", dataQR)
+      QRCode.toFile(dataQR, t.id + "", { errorCorrectionLevel: 'H' })
+    }
+  });
+
+  let a = [];
+
+
+  request.map(t => {
+    for (let i = 0; i < t.etiquetas; i++) {
+      console.log(`src="${dir}/${t.id}.png"`)
+      let copiar = `<td> <div> <img class="test1" src="${dir}/${t.id}.png" > </div> <div> id: ${t.id} <br>  ${t.nombre} </div> </td>`
+      //console.log(copiar)
+      a.push(copiar)
+    }
+  });
+
+  generatePDF(a, res);
+
+
+});
+
+function generatePDF(lista, resesponse) {
+
+
+  let temHtml = "<tr>";
+  let temo = 0;
+  for (let i = 1; i < lista.length + 1; i++) {
+    if (i % 6 === 0) {
+      temHtml += lista[temo] + "</tr> <tr>"
+    } else {
+      temHtml += lista[temo]
+    }
+    temo++;
+  }
+  temHtml += "</tr>"
+  let html = `<!DOCTYPE html><html><style>.test1 {height: 130px;width: 130px;}</style><head><mate charest="utf-8" /><title>Hello world!</title></head><body><table style="text-align: center;">${temHtml}</table></body></html>`
+
+  let myPath = path.join(__dirname, "../public/pdf/" + new Date().toLocaleDateString('en-US').replace("/", "-").replace("/", "-") + "/");
+
+
+  console.log("direcotiro ", myPath)
+  fs.mkdirSync(myPath, { recursive: true });
+
+  let name = `${new Date().toLocaleDateString('en-US').replace("/", "-").replace("/", "-")}-${Math.floor((Math.random() * 100000) + 1)}`
+  console.log(myPath + name + ".pdf")
+  pdf.create(html, options).toFile(myPath + "/" + name + ".pdf", function (err, res) {
+    if (err) return console.log(err);
+    console.log(res); // { filename: '/app/businesscard.pdf' }
+    resesponse.json({ code: 13, mensaje: name })
+  });
+
+}
+
 /**
  * 
  */
@@ -114,7 +214,6 @@ router.post('/devoluciones', async function (req, res, next) {
   const firm = "[index:devoluciones] "
   res.send({ mensaje: "hola mundo!!!! " })
 });
-
 
 
 async function verifyToken(token) {
