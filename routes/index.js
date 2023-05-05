c70d284421a7bf85ebdcba12b854234dc34bbd1e
Rodrigo <rodrigo.rivera.12357@gmail.com>
2023-05-05 00:50:09 -0600

    test3.6

--- routes/index.js
+++ routes/index.js
@@ -154,9 +154,9 @@ router.get('/pdf/qr', async (req, res, next) => {
 
   console.log("entrar a ver las imagenes: ")
 
-  let dir = path.join(__dirname, "../public/images/qr");
+  res.send("entre")
+  //let dir = path.join(__dirname, "../public/images/qr");
   // let files;
-  res.send({ dir })
   // try {
 
   //   files = await fs.promises.readdir(dir);
