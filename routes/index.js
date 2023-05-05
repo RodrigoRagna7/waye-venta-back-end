93d1ac448bb6edfa605c81fbc7a028ad65156b21
Rodrigo <rodrigo.rivera.12357@gmail.com>
2023-05-04 23:37:16 -0600

    test 3

--- routes/index.js
+++ routes/index.js
@@ -152,19 +152,19 @@ router.get('/pdf/qr', async (req, res, next) => {
   console.log("entrar a ver las imagenes: ",)
 
   let dir = path.join(__dirname, "../public/images/qr");
-  res.send({ request, dir })
-  // const files = await fs.promises.readdir(dir);
-  // console.log("at", files)
+  const files = await fs.promises.readdir(dir);
+  console.log("at", files)
 
   // request.map(t => {
-  //   if (fs.existsSync(dir + "/" + t.id + ".png")) {
-  //     console.log("existe " + dir + "/" + t.id + ".png")
-  //   } else {
-  //     let dataQR = dir + "/" + t.id + ".png";
-  //     console.log("url qr", dataQR)
-  //     QRCode.toFile(dataQR, t.id + "", { errorCorrectionLevel: 'H' })
-  //   }
-  // });
+  if (fs.existsSync(dir + "/" + t.id + ".png")) {
+    console.log("existe " + dir + "/" + t.id + ".png")
+  } else {
+    let dataQR = dir + "/" + t.id + ".png";
+    console.log("url qr", dataQR)
+    //QRCode.toFile(dataQR, t.id + "", { errorCorrectionLevel: 'H' })
+  }
+  res.send({ request, dir })
+});
 
   // let a = [];
 
